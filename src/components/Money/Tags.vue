86bdada42dbf5eca89292787e7d1026edc11a448
Guang-Yu6 <everyting-day@foxmail.com>
2021-03-31 15:57:09 +0800

    可以选中或取消 tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,17 +4,33 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)">{{tag}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
- export default {
-   name:'Tags'
+import Vue from 'vue';
+import {Component,Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class Tags extends Vue {
+    @Prop() dataSource:string[] | undefined;  // 告诉TS这是字符串数组
+    selectedTags: string[] = [];  // 选中后的
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    toggle(tag:string){  // 选中
+      const index = this.selectedTags.indexOf(tag);
+      if (index >= 0){
+        this.selectedTags.splice(index,1);
+      }else{
+        this.selectedTags.push(tag);
+      }
+
+    }
  }
 </script>
 
@@ -31,6 +47,7 @@
     display: flex;
     flex-wrap:wrap ;
     > li{
+      $bg:#d9d9d9;
       background: #d9d9d9;
       $h: 24px;
       height: $h;
@@ -39,6 +56,10 @@
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+      &.selected{
+        background: darken($bg,50%);
+        color: white;
+      }
     }
   }
   > .new {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,20 +1,27 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad/>
-    <Types :xxx="123"/>
+    <Types/>
     <Notes/>
-    <Tags/>
+    <Tags :data-source="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script lang="js">
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
+
   export default {
     name:'Money',
     components: {Tags, Notes, Types, NumberPad},
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    data() {
+      return {
+        tags:['衣','食','住','行','彩票']
+      }
+    }
   }
 </script>
 
